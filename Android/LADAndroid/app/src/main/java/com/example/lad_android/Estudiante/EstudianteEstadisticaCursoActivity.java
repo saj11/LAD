726bac3031b2b122627e3d565399ed84ddd6f80e
Kahho Chen <kahho.chen@gmail.com>
2019-05-27 10:38:58 -0600

    cambio vista estadistica Estudiante

--- Android/LADAndroid/app/src/main/java/com/example/lad_android/Estudiante/EstudianteEstadisticaCursoActivity.java
+++ Android/LADAndroid/app/src/main/java/com/example/lad_android/Estudiante/EstudianteEstadisticaCursoActivity.java
@@ -16,5 +16,13 @@ public class EstudianteEstadisticaCursoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estudiante_estadistica_curso);
+
+        mListView = (ListView)findViewById(R.id.EstudianteEstadisticaCursoListViewEstudiantes);
+        mTextPresente = (TextView) findViewById(R.id.EstudianteEstadisticaCursoNumeroPresenteTV);
+        mTextTardia = (TextView) findViewById(R.id.EstudianteEstadisticaCursoNumeroTardiaTV);
+        mTextAusente = (TextView) findViewById(R.id.EstudianteEstadisticaCursoNumeroAusenteTV);
+
+        
+
     }
 }
